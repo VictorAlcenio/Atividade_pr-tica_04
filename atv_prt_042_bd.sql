6783bdf699adc4da589ad1a4d4426d52a1493676
VictorAlcenio <48133710+VictorAlcenio@users.noreply.github.com>
2019-08-22 20:25:03 -0300

    desenvolvimento do banco de dados

---
+++ atv_prt_042_bd.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE teste1 (
+  testid int(4) NOT NULL AUTO_INCREMENT,
+  nome varchar(25) NOT NULL,
+  idade int(4) NOT NULL,
+PRIMARY KEY (testID)
+);
+
+
+INSERT INTO teste1 (nome, idade) VALUES
+
+('Victor', 16),
+('Jo�o', 30);
